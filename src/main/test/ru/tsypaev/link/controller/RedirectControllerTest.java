0384b67c71ba18bc2e9d1c041a45b7159b96afa2
Tsypaev Vladimir <tsypaevova@yandex.ru>
2018-12-29 00:02:07 +0500

    Controller test probe

--- src/main/test/ru/tsypaev/link/controller/RedirectControllerTest.java
+++ src/main/test/ru/tsypaev/link/controller/RedirectControllerTest.java
@@ -3,6 +3,8 @@ package ru.tsypaev.link.controller;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,10 +12,14 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.tsypaev.link.domain.Link;
+import ru.tsypaev.link.repository.LinkRepository;
 import ru.tsypaev.link.service.LinkService;
 
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -33,16 +39,23 @@ public class RedirectControllerTest {
     private LinkService linkService;
 
     @Autowired
+    private LinkRepository repository;
+
+    @Autowired
     EntityManager entityManager;
 
     @Before
-    public void setup() {
+    public void setup() throws URISyntaxException {
+
+        entityManager.persist(new Link("d9c9bc4c","https://www.yandex.ru"));
         this.mockMvc = standaloneSetup(new GenerateController(linkService)).build();
+//        Mockito.when(repository.findByLink("d9c9bc4c")).thenReturn(new Link("d9c9bc4c","https://www.yandex.ru"));
     }
 
     @Test
     public void shouldRedirect() throws Exception {
-        entityManager.persist(new Link("d9c9bc4c","https://www.yandex.ru"));
+        LinkService mock = org.mockito.Mockito.mock(LinkService.class);
+        Mockito.when(mock.getOriginalByShortUrl("d9c9bc4c")).thenReturn(new URI("https://www.yandex.ru"));
         this.mockMvc.perform(
                 get("/l/d9c9bc4c")
                 .accept(MediaType.APPLICATION_JSON_UTF8)
